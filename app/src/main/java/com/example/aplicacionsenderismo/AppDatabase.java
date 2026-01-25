91248102c24f5fee94de0b5c56b07b159573b53a
Sergio 104798 <sguillamon2005@gmail.com>
2026-01-25 12:02:46 +0100

    integracion de filtros por dificultad mostrados en un fragment

---
+++ app/src/main/java/com/example/aplicacionsenderismo/AppDatabase.java
@@ -0,0 +1,4 @@
+package com.example.aplicacionsenderismo;
+
+public abstract class AppDatabase {
+}
---
+++ app/src/main/java/com/example/aplicacionsenderismo/RutaDao.java
@@ -0,0 +1,31 @@
+package com.example.aplicacionsenderismo;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface RutaDao {
+
+    @Insert
+    void insertar(Ruta ruta);
+
+    @Delete
+    void eliminar(Ruta ruta);
+
+    @Update
+    void actualizar(Ruta ruta);
+
+    @Query("SELECT * FROM ruta ORDER BY id DESC")
+    List<Ruta> obtenerTodasRutas();
+
+    @Query("SELECT * FROM ruta WHERE dificultad = :dificultad")
+    List<Ruta> obtenerRutaPorDificultad();
+
+    @Query("SELECT * FROM ruta WHERE nombreRuta LIKE '%' || :nombreRuta || '%'")
+    List<Ruta> buscarRutaPorNombre();
+}
